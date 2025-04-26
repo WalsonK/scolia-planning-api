533f27dcb33c9ef7a9128f91f490d8b8de6c6aae
WalsonK <71435596+WalsonK@users.noreply.github.com>
2025-04-26 20:37:57 +0200

    FEAT: python - use greedy_planning

--- tester.py
+++ tester.py
@@ -9,5 +9,15 @@ if __name__ == "__main__":
     result = rustml.add(5, 3)
     print(f"Result from Rust: {result}")
 
-    data = fn.load_data()
-    print(f"Data loaded: {data}")
+    # data = fn.load_data()
+    # print(f"Data loaded: {data}")
+
+    test = rustml.generate_greedy_planning(
+        total_slots=7,
+        max_hours=35,
+        slot_minutes=90,
+        subjects=[0.0],
+        todo=[8.0],
+        unavailability=[[1.0]]
+    )
+    print(f"Test result: {test}")
